c78d5c2d6754b872d45e3a3cf862e66d38b19b36
Andrew <98813748+starboxxxx@users.noreply.github.com>
2024-12-10 22:45:56 +0900

    Title: 정수 복사, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241210/정수 복사/copy-integer.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        b = a;
+
+        int total = a * b;
+
+        System.out.printf("%d %d\n%d", a, b, total);
+    }
+}
