25d0300ce6dbf239c2dd6a8f6d92aed1c3edc222
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-01-07 01:01:09 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250107/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        System.out.print(exist(M, D));
+    }
+
+    public static String exist(int M, int D) {
+        if (M >= 1 && m <= 12) {
+            if (M == 2) {
+                if (D >=1 && D<=28) {
+                    return "Yes";
+                }
+                else {
+                    return "No";
+                }
+            }
+            else if (M == 1 || M==3 || M == 5 || M == 7 || M == 8 || M == 10 || M == 12) {
+                if (D >= 1 && D <= 31) {
+                    return "Yes";
+                }
+                else {
+                    return "No";
+                }
+            }
+            else {
+                if (D >= 1 && D <= 30) {
+                    return "Yes";
+                }
+                else {
+                    return "No";
+                }
+            }
+        }
+    }
+}
