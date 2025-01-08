af397e2cab2f0d93b5cbbec40c838225d8b887c3
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-01-09 00:36:33 +0900

    Title: 나누고 빼면서 합하기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250109/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] A = new int[n];
+
+        for (int i = 0; i<A.length; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        calculate(A, m);
+        
+    }
+
+    public static void calculate(int[] A, int m) {
+        int total = 0;
+        while (true) {
+            if (m == 1) {
+                total += A[m];
+                break;
+            }
+
+            else if (m % 2 == 1) {
+                total += A[m];
+                m -= 1;
+            }
+            else {
+                total += A[m];
+                m /= 2;
+            }
+        }
+        System.out.print(total);
+    }
+}
