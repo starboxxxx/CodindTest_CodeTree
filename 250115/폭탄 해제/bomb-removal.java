f59d2990ead9cd9eacfcbd75a1cdf2c72743a713
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-01-15 00:33:39 +0900

    Title: 폭탄 해제, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250115/폭탄 해제/bomb-removal.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Bomb {
+    String code;
+    char color;
+    int second;
+
+    public Bomb(String code, char color, int second) {
+        this.code = code;
+        this.color = color;
+        this.second = second;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String code = sc.next();
+        char color = sc.next().charAt(0);
+        int second = sc.nextInt();
+
+        Bomb bomb = new Bomb(code, color, second);
+
+        System.out.println("code : " + bomb.code);
+        System.out.println("color : " + bomb.color);
+        System.out.println("second : " + bomb.second);
+    }
+}
