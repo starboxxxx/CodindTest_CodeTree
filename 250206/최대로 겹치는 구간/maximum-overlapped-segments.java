e86e90ba8b22619d883b1416d9741815c997089c
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-02-06 01:12:35 +0900

    Title: 최대로 겹치는 구간, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250206/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] result = new int[200];
+
+
+        for (int i = 0; i<n; i++) {
+            int x1 = sc.nextInt()+100 - 1;
+            int x2 = sc.nextInt()+100 - 2;
+
+            for (int j = x1; j<=x2; j++) {
+                result[j]++;
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i<200; i++) {
+            if (result[i] > max) {
+                max = result[i];
+            }    
+        }
+
+        System.out.print(max);
+    }
+}
