4e79fb69bf846b282f5ba516bdff0ddfd507fb44
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-02-09 23:47:51 +0900

    Title: 만나는 그 순간, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250209/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] A = new int[2001];
+        int[] B = new int[2001];
+
+        int now = 1000;
+        int timeA = 1;
+        int timeB = 1;
+        for (int i = 0; i<N; i++) {
+            String d = sc.next();
+            int t = sc.nextInt();
+
+            for (int j=timeA; j<timeA+t; j++) {
+
+                if (d.equals("R")) {
+                        now++;
+                }
+                else {
+                    now--;
+                }
+                A[j] = now;
+            }
+            timeA += t;
+        }
+
+        now = 1000;
+        for (int i = 0; i<M; i++) {
+            String d = sc.next();
+            int t = sc.nextInt();
+
+            for (int j=timeB; j<timeB+t; j++) {
+                if (j!=0) {
+                    if (d.equals("R")) {
+                        now++;
+                    }
+                    else {
+                        now--;
+                    }
+                }
+                B[j] = now;
+            }
+            timeB += t;
+        }
+
+        for (int i = 1; i<=Math.min(timeA, timeB); i++) {
+            if (A[i] == 0 || B[i] == 0) {
+                System.out.print(-1);
+                break;
+            }
+            else if (A[i] == B[i]) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
