e388900f37a582787cf3dcfc291e61f1a5bf8d35
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-02-12 00:35:23 +0900

    Title: 문자에 따른 명령 2, Time: 164ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 250212/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = 0;
+        int y = 0;
+
+        int dir = 1;
+
+        int[] dirX = new int[]{1, 0, -1, 0};
+        int[] dirY = new int[]{0, -1, 0, 1};
+
+        String word = sc.next();
+
+        for (int i=0; i<word.length(); i++) {
+            char a = word.charAt(i);
+
+            if (a == 'L') {
+                dir = (dir + 1) % 4;
+            }
+            else if (a == 'R') {
+                dir = (dir + 3) % 4;
+            }
+            else {
+                x += dirX[dir];
+                y += dirY[dir];
+            }
+        } 
+
+        System.out.print(x + " " + y);
+    }
+}
