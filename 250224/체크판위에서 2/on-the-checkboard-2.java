09dd38b361c00b178ac066e20ea697f73c85dc04
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-02-24 01:58:14 +0900

    Title: 체크판위에서 2, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250224/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+
+        String[][] a = new String[R][C];
+
+        for (int i = 0; i<R; i++) {
+            for (int j = 0; j<C; j++) {
+                a[i][j] = sc.next();
+            }
+        }
+
+        String current = a[0][0];
+        int count = 0;
+
+        for (int i = 1; i<=R-3; i++) {
+            for (int j = 1; j<=C-3; j++) {
+                if (!a[i][j].equals(current)) {
+                    for (int k = j+1; k<=C-2; k++) {
+                        if (a[i][k].equals(current)) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.print(count);
+    }
+}
