030645647c9c1c8c651744fb64c6e9a0e298ac2e
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-06 00:38:50 +0900

    Title: 바구니 안의 사탕 2, Time: 118ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250306/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] candies = new int[100];
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            int position = sc.nextInt();
+            
+            if (candies[position] != 0) {
+                candies[position] += num;
+            }
+            else {
+                candies[position] = num;
+            }
+        }
+
+        int max = 0;
+
+        for (int i = 0; i<=99-(2*k); i++) {
+            int total = 0;
+            for (int j = i; j<=i + (2*k); j++) {
+                total += candies[j];
+            }
+
+            if (total > max) {
+                max = total;
+
+            }
+        }
+
+        System.out.print(max);
+    }
+}
