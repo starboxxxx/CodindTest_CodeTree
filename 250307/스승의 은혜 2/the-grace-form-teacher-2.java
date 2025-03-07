0ae2faae7b2825ac7fe13276eb674bbd4f5491d6
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-07 22:03:27 +0900

    Title: 스승의 은혜 2, Time: 164ms, Memory: 11MB, Status: Passed - CodeTree

--- 250307/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 250307/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,6 +10,8 @@ public class Main {
         for (int i = 0; i<n; i++) {
             price[i] = sc.nextInt();
         }
+
+        Arrays.sort(price);
 
         int max = 0;
 
