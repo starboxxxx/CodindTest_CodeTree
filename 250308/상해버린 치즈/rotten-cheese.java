50bd75614015197234cf19bcad3f9ffd33cd92e2
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-08 03:00:53 +0900

    Title: 상해버린 치즈, Time: 169ms, Memory: 11MB, Status: Passed - CodeTree

--- 250308/상해버린 치즈/rotten-cheese.java
+++ 250308/상해버린 치즈/rotten-cheese.java
@@ -22,21 +22,39 @@ public class Main {
 
         for (int i = 0; i < S; i++) {
             
-            sick[i][0] = sc.nextInt();
-            sick[i][1] = sc.nextInt();
+            int p = sc.nextInt();
+            int t = sc.nextInt();
+
+            sick[i][0] = p;
+            sick[i][1] = t;
         }
 
-        for (int i = 0; i<D; i++) {
+        int[] cheese = new int[M+1];
+
+        for (int i = 1; i<=M; i++) {
+            boolean isBad = true;
             for (int j = 0; j<S; j++) {
                 int p = sick[j][0];
                 int t = sick[j][1];
 
-                if (eat[i][0] == p && eat[i][2] <= t-1) {
-                    int m = eat[i][1];
+                for (int z = 0; z<D; z++) {
+                    if (eat[z][1] == i
+                    && eat[z][0] == p && eat[z][2] <= t-1) {
+                        break;
+                    }
+                    else if (z == D-1) {
+                        isBad = false;
+                    }
+                }
 
-                    for (int z = 0; z<D; z++) {
-                        if (eat[z][1] == m) {
-                            people[eat[z][0]] = 1;
+                if (isBad == false) {
+                    break;
+                }
+                
+                if (j == S-1) {
+                    for (int h = 0; h<D; h++) {
+                        if (eat[h][1] == i) {
+                            people[eat[h][0]] = 1;
                         }
                     }
                 }
