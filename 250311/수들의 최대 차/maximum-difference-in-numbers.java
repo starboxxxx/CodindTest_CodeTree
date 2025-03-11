99aace82591c19135421fddcbea3111e11175700
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-11 20:26:11 +0900

    Title: 수들의 최대 차, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250311/수들의 최대 차/maximum-difference-in-numbers.java
+++ 250311/수들의 최대 차/maximum-difference-in-numbers.java
@@ -11,16 +11,27 @@ public class Main {
 
         Arrays.sort(arr);
         
-        for (int i = 1; i<n; i++) {
-            int max = 0;
-            int min = Integer.MAX_VALUE;
-            for (int j = 0; j<=i; j++) {
-                max = Math.max(max, arr[j]);
-                min = Math.min(min, arr[j]);
+        boolean isTrue = true;
+        for (int i = 2; i<=n; i++) {
+            for (int j = 0; j<=n-i; j++) {
+                int max = 0;
+                int min = Integer.MAX_VALUE;
+                for (int z = j; z<j+i; z++) {
+                    max = Math.max(max, arr[z]);
+                    min = Math.min(min, arr[z]);
+                }
+
+                if (max - min <= k) {
+                    break;
+                }
+
+                if (j == n-i) {
+                    isTrue = false;
+                }
             }
 
-            if (max - min > k) {
-                System.out.print(i);
+            if (isTrue == false) {
+                System.out.print(i-1);
                 break;
             }
         }
