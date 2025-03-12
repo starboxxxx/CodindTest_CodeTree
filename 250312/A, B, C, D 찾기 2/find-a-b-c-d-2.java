aeffe17e980154f29b41cdc86a2089532255a365
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-12 18:33:48 +0900

    Title: A, B, C, D 찾기 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250312/A, B, C, D 찾기 2/find-a-b-c-d-2.java
+++ 250312/A, B, C, D 찾기 2/find-a-b-c-d-2.java
@@ -4,30 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[15];
+        int max = 0;
         for (int i = 0; i < 15; i++) {
             arr[i] = sc.nextInt();
+            max = Math.max(max, arr[i]);
         }
 
         Arrays.sort(arr);
 
         int A = arr[0];
         int B = arr[1];
+        int C = 0;
 
-        Boolean isTrue = false;
-        
-        for (int i = 2; i<14; i++) {
-            for (int j = i+1; j<15; j++) {
-                if (A + arr[i] + arr[j] == arr[12]
-                && B + arr[i] + arr[j] == arr[13]
-                && A + B + arr[i] + arr[j] == arr[14]) {
-                    System.out.print(A + " " + B + " " + arr[i] + " " + arr[j]);
-                    isTrue = true;
-                    break;
-                }
-            }
-            if (isTrue) {
-                break;
-            }
+        if (A+B == arr[2]) {
+            C = arr[3];
         }
+
+        else {
+            C = arr[2];
+        }
+
+        int D = arr[14] - A - B - C;
+
+        System.out.print(A + " " + B + " " + C + " " + D);
     }
 }
