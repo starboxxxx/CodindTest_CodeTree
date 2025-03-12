525fb3785f9201b1badd5b37bfef0c4c80655ba8
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-12 19:49:10 +0900

    Title: 이상한 폭탄 3, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250312/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] bombs = new int[n];
+        for (int i = 0; i < n; i++){
+            bombs[i] = sc.nextInt();
+        }
+        int max = 0;
+        int answer = 0;
+        for(int j = 0; j<n-1; j++) {
+            int current = j;
+            int count = 0;
+            for (int z = j+1; z<n; z++) {
+                if (bombs[z] == bombs[j] && z - current <= k) {
+                    count++;
+                    current = z;
+                }
+            }
+
+            if (count > max) {
+                max = count;
+                answer = bombs[j];
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
