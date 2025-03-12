07730f2ef7c0ece22dfd162c63ca5e96679cb40e
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-12 19:28:20 +0900

    Title: 초기 수열 복원하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250312/초기 수열 복원하기/restore-initial-sequence.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n-1];
+
+        for (int i = 0; i<n-1; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i<n; i++) {
+            boolean isTrue = true;
+            int count = 0;
+            int[] num = new int[n];
+            num[0] = i;
+            for (int j = 0; j<n-1; j++) {
+                if (arr[j] - num[j] <= n) {
+                    int x = arr[j] - num[j];
+                    for (int z = 0; z<n; z++) {
+                        if (x == num[z]) {
+                            isTrue = false;
+                            break;
+                        }
+
+                        if (z == n-1) {
+                            num[j+1] = x;
+                        }
+                    }
+                    if (j == n-2 && isTrue) {
+                        for (int z = 0; z<n; z++) {
+                            System.out.print(num[z] + " ");
+                        }
+                    }
+                }
+            }
+        }
+        
+
+    }
+}
