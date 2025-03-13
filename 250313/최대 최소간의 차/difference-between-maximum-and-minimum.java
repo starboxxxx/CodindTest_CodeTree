c55c238d32ab90918bcaadf1b06801dde8190702
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-13 20:27:44 +0900

    Title: 최대 최소간의 차, Time: 3067ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 250313/최대 최소간의 차/difference-between-maximum-and-minimum.java
+++ 250313/최대 최소간의 차/difference-between-maximum-and-minimum.java
@@ -5,12 +5,16 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] arr = new int[n];
+        int M = 0;
+        int L = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
+            M = Math.max(M, arr[i]);
+            L = Math.min(L, arr[i]);
         }
         
         int min = Integer.MAX_VALUE;
-        for (int i = 1; i<=10000; i++) {
+        for (int i = L; i<=M; i++) {
             for (int j = i; j<=i+k; j++) {
                 if (j<= 10000) {
                     int cost = 0;
