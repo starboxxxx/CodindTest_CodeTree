73f7eea13d989343522208c11db49cf2dc7ebf5d
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-13 18:45:09 +0900

    Title: 구간 잘 나누기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250313/구간 잘 나누기/divide-sections-well.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] num = new int[n];
+
+        int max = 0;
+        for (int i = 0; i<n; i++) {
+            num[i] = sc.nextInt();
+            max = Math.max(max, num[i]);
+        }
+        
+        for (int i = max+1; i<200; i++) {
+            int k = i;
+            int sum = 0;
+            int count = 0;
+            for (int j = 0; j<n; j++) {
+                if (num[j] + sum >= k) {
+                    count++;
+                    sum = num[j];
+                }
+                else {
+                    
+                    sum += num[j];
+                }
+            }
+            if (count == m) {
+                System.out.print(k);
+                break;
+            }
+        }
+    }
+}
