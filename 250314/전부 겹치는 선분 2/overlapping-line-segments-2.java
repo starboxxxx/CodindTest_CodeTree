65331a0356c225cedc35bb3cc10490e0a29e8f12
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-14 20:07:42 +0900

    Title: 전부 겹치는 선분 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250314/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i<n; i++) {
+            int start = 0;
+            int end = Integer.MAX_VALUE;
+            boolean isTrue = false;
+            for (int j = 0; j<n; j++) {
+                if (j == i) {
+                    continue;
+                }
+
+                if (x1[j] > end || x2[j] < start) {
+                    break;
+                }
+                else {
+                    if (i == n-1) {
+                        System.out.print("Yes");
+                        isTrue = true;
+                        break;
+                    }
+                    start = Math.max(start, x1[j]);
+                    end = Math.min(end, x2[j]);
+                }
+            }
+            if (isTrue) {
+                break;
+            }
+
+
+            if (i == n-1) {
+                System.out.print("No");
+            }
+        }
+    }
+}
