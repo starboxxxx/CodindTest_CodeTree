50fb724d53679a39f69438945aad9d4595a59121
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-17 19:42:35 +0900

    Title: X 달리기, Time: 159ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250317/X 달리기/x-run.java
+++ 250317/X 달리기/x-run.java
@@ -7,20 +7,22 @@ public class Main {
         int distance = 2;
         int time = 2;
 
-        for (int i = 10000; i>=0; i--) {
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 10000; i>=2; i--) {
             int total = distance;
+            int t = time;
             for (int j = 2; j<=i; j++) {
                 total += j * 2;
             }
 
             if (total + i+1 <= x) {
-                System.out.print(i);
-                time += (i-1) * 2 + 1;
-                time += x - (total + i + 1);
-                break;
+                t += (i-1) * 2 + 1;
+                t += x - (total + i + 1);
+                min = Math.min(min, t);
             }
         }
 
-        System.out.print(time);
+        System.out.print(min);
     }
 }
