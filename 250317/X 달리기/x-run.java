c29aee0907e4d547be9da1521784db03b41e660f
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-03-17 19:11:23 +0900

    Title: X 달리기, Time: 3070ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 250317/X 달리기/x-run.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        int distance = 0;
+        int time = 0;
+
+        if (x % 2 == 0) {
+            distance = 3;
+            time = 3;
+        }
+        else {
+            distance = 2;
+            time = 2;
+        }
+
+        for (int i = 2; i<Integer.MAX_VALUE; i++) {
+            int total = distance;
+            for (int j = 2; j<=i; j++) {
+                total += j * 2;
+            }
+
+            if (total + i+1 == x) {
+                time += (i-1) * 2 + 1;
+                break;
+            }
+        }
+
+        System.out.print(time);
+    }
+}
