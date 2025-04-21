07c14a11445e9f28ce23fb78b32821d4375a6b74
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-04-21 22:21:02 +0900

    Title: 피보나치 수, Time: 120ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250421/피보나치 수/fibonacci-number.java
+++ 250421/피보나치 수/fibonacci-number.java
@@ -9,25 +9,15 @@ public class Main {
         n = sc.nextInt();
         
         num = new int[n];
-
-        for (int i = 0; i<n; i++) {
-            num[i] = -1;
-        }
-
+        num[0] = 1;
+        num[1] = 1;
         System.out.print(dp(n-1));
     }
 
     public static int dp(int k) {
 
-        if (num[k] != -1) {
-            return num[k];
-        }
-
-        if (k <= 1) {
-            num[k] = 1;
-        }
-        else {
-            num[k] = dp(k-1) + dp(k-2);
+        for (int i = 2; i<n; i++) {
+            num[i] = num[i-1] + num[i-2];
         }
 
         return num[k];
