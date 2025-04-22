4ec195c01a110fe04887b4615e8da8acee63fc6f
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-04-22 20:42:43 +0900

    Title: 사각형 채우기, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 250422/사각형 채우기/rectangle-fill.java
+++ 250422/사각형 채우기/rectangle-fill.java
@@ -12,11 +12,9 @@ public class Main {
         count[1] = 1;
 
         for (int i = 2; i<=n; i++) {
-            count[i] = count[i-1] + count[i-2];
+            count[i] = (count[i-1] + count[i-2]) % 10007;
         }
 
-        System.out.print(count[n] % 10007);
-
-        
+        System.out.print(count[n]);
     }
 }
