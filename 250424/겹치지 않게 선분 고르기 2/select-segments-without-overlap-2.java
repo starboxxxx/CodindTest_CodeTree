a674ef7da79536245bc38c455fad9bdde95be492
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-04-24 00:49:53 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 230ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 250424/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int n;
+    public static int[][] segments;
+    public static int[] count;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        segments = new int[n][2];
+        count = new int[n];
+        for (int i = 0; i < n; i++) {
+            segments[i][0] = sc.nextInt();
+            segments[i][1] = sc.nextInt();
+            count[i] = 1;
+        }
+
+        System.out.print(dp());
+
+    }
+
+    public static int dp() {
+
+        for (int i = 1; i<n; i++) {
+            int start = segments[i][0];
+            int end = segments[i][1];
+
+            for (int j = 0; j<i; j++) {
+                int s = segments[j][0];
+                int e = segments[j][1];
+
+                if (s > end || e < start) {
+                    count[i] = Math.max(count[i], count[j] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i<n; i++) {
+            max = Math.max(max, count[i]);
+        }
+
+        return max;
+    }
+}
