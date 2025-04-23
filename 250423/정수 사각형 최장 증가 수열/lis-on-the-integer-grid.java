f9cc0671dd4ed2465cb5cafb4f12c29218de5fb8
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-04-23 21:16:38 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250423/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 250423/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -1,22 +1,9 @@
 import java.util.*;
 
-class Point {
-    int x;
-    int y;
-    int count;
-
-    public Point(int x, int y, int count) {
-        this.x = x;
-        this.y = y;
-        this.count = count;
-    }
-}
-
 public class Main {
 
     public static int n;
     public static int[][] grid;
-    public static Queue<Point> q = new LinkedList<>();
     public static int[] dx = new int[]{-1, 0, 1, 0};
     public static int[] dy = new int[]{0, 1, 0, -1};
     public static int[][] visited;
@@ -36,34 +23,38 @@ public class Main {
         boolean isMax = false;
         for (int i = 0; i<n; i++) {
             for (int j = 0; j<n; j++) {
-                if (visited[i][j] == 0) {
-                    visited[i][j] = 1;
-                    dfs(i, j, 1);
-                }
+                int answer = dfs(i, j);
+                max = Math.max(max, answer);
             }
         }
 
         for (int i = 0; i<n; i++) {
             for (int j = 0; j<n; j++) {
-                max = Math.max(max, visited[i][j]);
+                System.out.print(visited[i][j] + " ");
             }
+            System.out.println();
         }
-
-        System.out.print(max);
 
     }
 
 
-    public static void dfs(int x, int y, int count) {
+    public static int dfs(int x, int y) {
+        if (visited[x][y] != 0) {
+            return visited[x][y];
+        }
+
+        visited[x][y] = 1;
         for (int i = 0; i<4; i++) {
             int newX = x + dx[i];
             int newY = y + dy[i];
 
-            if (canGo(x, y, newX, newY, count+1)) {
-                visited[newX][newY] = count+1;
-                dfs(newX, newY, count+1);
+            if (newX >= 0 && newX < n && newY >= 0 && newY < n
+            && grid[newX][newY] > grid[x][y]) {
+                visited[x][y] = Math.max(visited[x][y], dfs(newX, newY)+1);
             }
         }
+        return visited[x][y];
+
     }
 
     public static boolean canGo (int x, int y, int newX, int newY, int num) {
