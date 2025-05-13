584ac70e0bd2c38b6690d9a6b656dc96bad67f60
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-14 00:11:50 +0900

    Title: 자리 차지하기, Time: 3072ms, Memory: 132MB, Status: Time Limit Exceed - Codetree

---
+++ 250514/자리 차지하기/take-place.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        TreeSet<Integer> set = new TreeSet<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int chair = sc.nextInt();
+
+            if (set.contains(chair)) {
+                for (int j =chair-1; j>=1; j--) {
+                    if (!set.contains(j)) {
+                        set.add(j);
+                        count++;
+                        break;
+                    }
+                }
+            }
+            else {
+                set.add(chair);
+                count++;
+            }
+        }
+        
+        System.out.print(count);
+    }
+}
