8520787d49ded7cfce9a8b03d26bbea78786d8a0
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-14 00:17:02 +0900

    Title: 자리 차지하기, Time: 821ms, Memory: 27MB, Status: Wrong Answer - Codetree

--- 250514/자리 차지하기/take-place.java
+++ 250514/자리 차지하기/take-place.java
@@ -6,9 +6,10 @@ public class Main {
         int m = sc.nextInt();
         TreeSet<Integer> set = new TreeSet<>();
         int count = 0;
+        boolean can = true;
         for (int i = 0; i < n; i++) {
             int chair = sc.nextInt();
-
+            
             if (set.contains(chair)) {
                 for (int j =chair-1; j>=1; j--) {
                     if (!set.contains(j)) {
@@ -16,7 +17,15 @@ public class Main {
                         count++;
                         break;
                     }
+                    else if (j == 1) {
+                        can = false;
+                    }
                 }
+
+                if (can == false) {
+                    break;
+                }
+
             }
             else {
                 set.add(chair);
