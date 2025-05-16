2ba6064a062bc86b1b99a8a4b21b617e6b5cf42d
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-16 20:27:04 +0900

    Title: 최솟값 3개, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250516/최솟값 3개/top-3-smallest-number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            pq.add(sc.nextInt());
+
+            if (pq.size() < 3) {
+                System.out.println(-1);
+            }
+            else {
+                int a = pq.poll();
+                int b = pq.poll();
+                int c = pq.poll();
+
+                System.out.println(a * b * c);
+                pq.add(a);
+                pq.add(b);
+                pq.add(c);
+            }
+        }
+    }
+}
