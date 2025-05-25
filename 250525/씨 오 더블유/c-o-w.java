fde9a3ed111345e246845fd69ae3f6e2a1271787
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-25 20:52:26 +0900

    Title: 씨 오 더블유, Time: 163ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250525/씨 오 더블유/c-o-w.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int n;
+    public static int[][] nums;
+    public static char[] alpha;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        String str = sc.next();
+        alpha = new char[n+1];
+
+        for (int i = 1; i<=n; i++) {
+            alpha[i] = str.charAt(i-1);
+        }
+
+        nums = new int[n+1][3];
+
+        System.out.print(dp());
+    }
+
+    public static int dp() {
+        for (int i = 1; i<=n; i++) {
+            char a = alpha[i];
+
+            nums[i][0] = nums[i-1][0];
+            nums[i][1] = nums[i-1][1];
+            nums[i][2] = nums[i-1][2];
+
+            if (a == 'C') {
+                nums[i][0] = nums[i-1][0] + 1;
+            }
+
+            else if (a == 'O') {
+                nums[i][1] += nums[i-1][0]; 
+            }
+
+            else {
+                nums[i][2] += nums[i-1][1];
+            }
+        }
+
+        return nums[n][2];
+    }
+}
