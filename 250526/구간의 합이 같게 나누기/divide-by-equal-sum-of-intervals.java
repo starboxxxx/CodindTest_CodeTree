e0aa4422c21a4221cf6c79de47e346dc710225a9
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-26 21:06:31 +0900

    Title: 구간의 합이 같게 나누기, Time: 599ms, Memory: 33MB, Status: Passed - Codetree

--- 250526/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.java
+++ 250526/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.java
@@ -6,41 +6,61 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         long[] arr = new long[n];
+        long total = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextLong();
+            total += arr[i];
         }
         
-        long[] sum = new long[n];
-
-        long[] L = new long[n];
-        long[] R = new long[n];
-
-        L[0] = arr[0];
-        for (int i = 1; i<n; i++) {
-            L[i] = L[i-1] + arr[i];
+        long k;
+        if (total % 4 != 0) {
+            System.out.print(0);
         }
+        else {
+            k = total / 4;
 
-        R[n-1] = arr[n-1];
-        for (int i = n-2; i>=0; i--) {
-            R[i] = R[i+1] + arr[i];
-        }
-        
-        int answer = 0;
-        for (int i = 1; i<=n-2; i++) {
-            
-            int pivot = i;
+            long[] L = new long[n];
+            long[] R = new long[n];
 
-            for (int j = 0; j<pivot; j++) {
-                if (L[j] == L[pivot] - L[j]) {
-                    for (int z = pivot+2; z<=n-1; z++) {
-                        if (R[z] == R[pivot+1] - R[z]) {
-                            answer++;
-                        }
-                    }
+            L[0] = 0;
+            long sum = arr[0];
+            long cnt = (sum == k) ? 1: 0;
+
+            for (int i = 1; i<n; i++) {
+                sum += arr[i];
+
+                if (sum == 2 * k) {
+                    L[i] = cnt;
+                }
+
+                if (sum == k) {
+                    cnt++;
                 }
             }
-        }
 
-        System.out.print(answer);
+            R[n-1] = arr[n-1];
+            sum = arr[n-1];
+            cnt = (sum == k) ? 1: 0;
+
+            for (int i = n-2; i>=0; i--) {
+                sum += arr[i];
+
+                if (sum == 2 * k) {
+                    R[i] = cnt;
+                }
+
+                if (sum == k) {
+                    cnt++;
+                }
+            }
+
+            long ans = 0;
+
+            for (int i = 1; i<n-1; i++) {
+                ans += (long) L[i] * R[i+1];
+            }
+            System.out.print(ans);
+            
+        }
     }
 }
