59511efaa4d245710a6f6f87c5d438cf6fffb356
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-27 20:50:51 +0900

    Title: 최대 구간의 크기, Time: 1395ms, Memory: 59MB, Status: Passed - Codetree

---
+++ 250527/최대 구간의 크기/size-of-largest-interval.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Point implements Comparable<Point> {
+    int x;
+    int v;
+    int index;
+
+    public Point (int x, int v, int index) {
+        this.x = x;
+        this.v = v;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(Point p) {
+        return this.x - p.x;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        ArrayList<Point> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            list.add(new Point(a, 1, i));
+            list.add(new Point(b, -1, i));
+        }
+
+        Collections.sort(list);
+        TreeSet<Integer> set = new TreeSet<>();
+        int start = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i<2*n; i++) {
+            int x = list.get(i).x;
+            int v = list.get(i).v;
+            int index = list.get(i).index;
+
+            if (v == 1) {
+                if (set.isEmpty()) {
+                    start = x;
+                }
+                set.add(index);
+            }
+
+            else {
+                if (set.size() == 1) {
+                    max = Math.max(max, x - start);
+                }
+                set.remove(index);
+            }
+        }
+
+        System.out.print(max);
+    }
+}
