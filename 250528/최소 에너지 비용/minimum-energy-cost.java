c00a93877186af5113c8d849110f6600223469a9
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-28 22:13:45 +0900

    Title: 최소 에너지 비용, Time: 242ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250528/최소 에너지 비용/minimum-energy-cost.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dist = new int[n];
+        int[] cost = new int[n];
+        for (int i = 0; i < n-1; i++)
+            dist[i] = sc.nextInt();
+        for (int i = 0; i < n; i++)
+            cost[i] = sc.nextInt();
+        
+        int[] R = new int[n];
+        
+        for (int i = n-2; i>=0; i--) {
+            R[i] = R[i+1] + dist[i];
+        }
+
+        int min = Integer.MAX_VALUE;
+        int total = 0;
+        for (int i = 0; i<n; i++) {
+            
+            if (i == 0) {
+                total = 0;
+            }
+            else {
+                total += dist[i-1] * cost[i-1];
+            }
+            min = Math.min(min, total + cost[i] * R[i]);
+        }
+        
+        System.out.print(min);
+    }
+}
