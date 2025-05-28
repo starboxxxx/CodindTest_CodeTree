946ba35067a7783705d7d60752f3d910cce6edee
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-28 22:20:24 +0900

    Title: 최소 에너지 비용, Time: 209ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250528/최소 에너지 비용/minimum-energy-cost.java
+++ 250528/최소 에너지 비용/minimum-energy-cost.java
@@ -3,30 +3,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] dist = new int[n];
-        int[] cost = new int[n];
+        long[] dist = new long[n];
+        long[] cost = new long[n];
         for (int i = 0; i < n-1; i++)
-            dist[i] = sc.nextInt();
+            dist[i] = (long) sc.nextInt();
         for (int i = 0; i < n; i++)
-            cost[i] = sc.nextInt();
+            cost[i] = (long) sc.nextInt();
         
-        int[] R = new int[n];
+        long[] R = new long[n];
         
         for (int i = n-2; i>=0; i--) {
             R[i] = R[i+1] + dist[i];
         }
 
-        long min = (long)Integer.MAX_VALUE;
+        long min = cost[0] * R[0];
         long total = 0;
-        for (int i = 0; i<n; i++) {
-            
-            if (i == 0) {
-                total = 0;
-            }
-            else {
-                total += (long)dist[i-1] * cost[i-1];
-            }
-            min = Math.min(min, total + (long)cost[i] * R[i]);
+        for (int i = 1; i<n; i++) {
+            total += dist[i-1] * cost[i-1];
+            min = Math.min(min, total + cost[i] * R[i]);
         }
         
         System.out.print(min);
