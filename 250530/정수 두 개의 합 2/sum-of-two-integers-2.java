f27eff5b1b710080495f80b6b2bee67c56a653de
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-30 22:40:53 +0900

    Title: 정수 두 개의 합 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250530/정수 두 개의 합 2/sum-of-two-integers-2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+    
+        int count = 0;
+        int end = n;
+
+        for (int i = 0; i<n; i++) {
+            int sum = arr[i];
+            int j = i;
+
+            while (j+1 < end && sum + arr[j+1] <= k) {
+                count++;
+                j++;
+            }
+
+            end = j;
+        }
+
+        System.out.print(count);
+    }
+}
