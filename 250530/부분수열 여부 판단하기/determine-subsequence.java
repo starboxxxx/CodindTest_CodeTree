91edd241a93de04539e01b1624621f33e61dd09a
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-30 21:59:28 +0900

    Title: 부분수열 여부 판단하기, Time: 1021ms, Memory: 51MB, Status: Runtime Error - Codetree

---
+++ 250530/부분수열 여부 판단하기/determine-subsequence.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] A = new int[n+1];
+        int[] B = new int[n+1];
+
+        for (int i = 1; i<=n; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i<=m; i++) {
+            B[i] = sc.nextInt();
+        }
+
+        int j = 1;
+        boolean can = true;
+        for (int i = 1; i<=m; i++) {
+
+            while (j <= n && B[i] != A[j]) {
+                j++;
+            }
+
+            if (j == n+1) {
+                can = false;
+                break;
+            }
+            else {
+                j++;
+            }
+        }
+
+        if (can) {
+            System.out.print("Yes");
+        } 
+        else {
+            System.out.print("No");
+        }
+    }
+}
