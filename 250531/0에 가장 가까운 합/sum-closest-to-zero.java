e210315553fbd5cf0ba69a49181551c84aaf3f78
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-05-31 21:31:44 +0900

    Title: 0에 가장 가까운 합, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250531/0에 가장 가까운 합/sum-closest-to-zero.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+
+        int index = n-1;
+
+        int min = Math.abs(arr[0] + arr[n-1]);
+
+        for (int i = 0; i<n; i++) {
+
+            if (index <= i) {
+                break;
+            }
+
+            while (index-1 >= 0 && Math.abs(arr[i] + arr[index-1]) < Math.abs(arr[i] + arr[index])) {
+                min = Math.min(min, Math.abs(arr[i] + arr[index-1]));
+                index--;
+            }
+        }
+
+        System.out.print(min);
+    }
+}
