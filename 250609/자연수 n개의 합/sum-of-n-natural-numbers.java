e9051a7f3821c169eeae4cfa8721fc6c74f6b204
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-06-09 20:32:42 +0900

    Title: 자연수 n개의 합, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250609/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long s = sc.nextLong();
+
+        long max = Long.MIN_VALUE;
+        long left = 1;
+        long right = 1000000000;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (mid * (mid+1) / 2 <= s) {
+                left = mid+1;
+                max = Math.max(max, mid);
+            }
+            else {
+                right = mid-1;
+            }
+        }
+
+        System.out.print(max);
+        
+    }
+}
