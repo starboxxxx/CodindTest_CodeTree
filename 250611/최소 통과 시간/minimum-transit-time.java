75ede609441d969666f73247a6396bae98a22b70
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-06-11 00:07:47 +0900

    Title: 최소 통과 시간, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250611/최소 통과 시간/minimum-transit-time.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++)
+            arr[i] = sc.nextInt();
+        
+        int left = 1;
+        int right = 1000000000;
+        int answer = Long.MAX_VALUE;
+
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            int count = 0;
+
+            for (int i = 0; i<m; i++) {
+                count += (mid / arr[i]);
+            }
+
+            if (count >= n) {
+                right = mid-1;
+                answer = Math.min(answer, mid);
+            }
+            else {
+                left = mid+1;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
