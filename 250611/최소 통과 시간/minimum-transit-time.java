aa03fbdafddc3254913fbd9a42b3c5d2efaae7b2
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-06-11 00:27:23 +0900

    Title: 최소 통과 시간, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

--- 250611/최소 통과 시간/minimum-transit-time.java
+++ 250611/최소 통과 시간/minimum-transit-time.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        long n = sc.nextLong();
         int m = sc.nextInt();
         long[] arr = new long[m];
         for (int i = 0; i < m; i++)
