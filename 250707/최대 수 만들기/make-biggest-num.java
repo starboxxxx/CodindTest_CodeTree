2463203b204387427407b70ce51af357127b8b6e
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-07-07 23:54:53 +0900

    Title: 최대 수 만들기, Time: 251ms, Memory: 21MB, Status: Runtime Error - Codetree

---
+++ 250707/최대 수 만들기/make-biggest-num.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Number implements Comparable<Number> {
+    int num;
+
+    public Number (int num) {
+        this.num = num;
+    }
+
+    @Override
+    public int compareTo(Number n) {
+        int a = this.num;
+        int b = n.num;
+
+        int x = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int y = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+
+        return x - y;
+    } 
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Number[] num = new Number[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = new Number(sc.nextInt());
+        }
+        
+        Arrays.sort(num);
+
+        for (int i = n-1; i>=0; i--) {
+            System.out.print(num[i].num);
+        }
+    }
+}
