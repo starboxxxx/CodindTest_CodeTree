abef13d7bc0d9463c8918a57854aaff76b64fe42
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-07-16 22:19:38 +0900

    Title: 폭탄 해체 작업, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250716/폭탄 해체 작업/the-bomb-dismantling.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Bomb implements Comparable<Bomb> {
+    int score;
+    int time;
+
+    public Bomb (int score, int time) {
+        this.score = score;
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(Bomb b) {
+        return b.score - this.score;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] time = new int[1001];
+        Bomb[] bombs = new Bomb[n];
+        for (int i = 0; i < n; i++) {
+            int score = sc.nextInt();
+            int timeLimit = sc.nextInt();
+
+            bombs[i] = new Bomb(score, timeLimit);
+        }
+
+        int total = 0;
+        Arrays.sort(bombs);
+        for (int i = 0; i<n; i++) {
+            if (time[bombs[i].time] != 1) {
+                time[bombs[i].time] = 1;
+                total += bombs[i].score;
+            }
+        }
+
+        System.out.print(total);
+    }
+}
