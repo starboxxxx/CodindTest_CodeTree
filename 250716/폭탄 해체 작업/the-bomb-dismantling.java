08d8bf686eec596cc2116d4c6ae60a13661a4789
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-07-16 22:32:24 +0900

    Title: 폭탄 해체 작업, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250716/폭탄 해체 작업/the-bomb-dismantling.java
+++ 250716/폭탄 해체 작업/the-bomb-dismantling.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] time = new int[1001];
+        int[] time = new int[1000];
         Bomb[] bombs = new Bomb[n];
         for (int i = 0; i < n; i++) {
             int score = sc.nextInt();
@@ -32,11 +32,13 @@ public class Main {
         }
 
         int total = 0;
+        int current = 0;
         Arrays.sort(bombs);
         for (int i = 0; i<n; i++) {
-            if (time[bombs[i].time-1] != 1) {
-                time[bombs[i].time-1] = 1;
+            if (bombs[i].time > current && time[current] != 1) {
+                time[current] = 1;
                 total += bombs[i].score;
+                current++;
             }
         }
 
