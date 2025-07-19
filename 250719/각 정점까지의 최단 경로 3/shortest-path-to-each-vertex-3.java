b31b2e28f3b632ee1e70aec226b612e422f2fe42
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-07-19 21:44:30 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250719/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] vertex = new int[n+1][n+1];
+        int[] visited = new int[n+1];
+        int[] dist = new int[n+1];
+
+        for (int i = 1; i<=n; i++) {
+            dist[i] = (int)1e9;
+        }
+        dist[1] = 0;
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+
+            vertex[u][v] = w;
+        }
+
+
+        
+        for (int i = 1; i<=n; i++) {
+            int minIndex = -1;
+
+            for (int j = 1; j<=n; j++) {
+                if (visited[j] == 1) {
+                    continue;
+                }
+
+                if (minIndex == -1 || dist[minIndex] > dist[j]) {
+                    minIndex = j;
+                }
+            }
+
+            visited[minIndex] = 1;
+
+            for (int j = 1; j<=n; j++) {
+                if (vertex[minIndex][j] == 0) {
+                    continue;
+                }
+
+                dist[j] = Math.min(dist[j], dist[minIndex] + vertex[minIndex][j]);
+            }
+        }
+
+        for (int i = 2; i<=n; i++) {
+            System.out.println(dist[i]);
+        }
+    }
+}
