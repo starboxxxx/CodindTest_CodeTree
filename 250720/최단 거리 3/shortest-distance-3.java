d993dc4dc720756cc34eb96be98ad16c175452a4
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-07-20 22:23:59 +0900

    Title: 최단 거리 3, Time: 767ms, Memory: 34MB, Status: Passed - Codetree

---
+++ 250720/최단 거리 3/shortest-distance-3.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Node {
+    int index;
+    int dist;
+
+    public Node (int index, int dist) {
+        this.index = index;
+        this.dist = dist;
+    }
+}
+
+class Element implements Comparable<Element> {
+    int dist;
+    int index;
+
+    public Element(int dist, int index) {
+        this.dist = dist;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(Element e) {
+        return this.dist - e.dist;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        ArrayList<Node>[] graph = new ArrayList[n+1];
+
+        for (int i = 1; i<=n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            graph[u].add(new Node(v, w));
+            graph[v].add(new Node(u, w));
+        }
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        PriorityQueue<Element> pq = new PriorityQueue<>();
+
+        int[] dist = new int[n+1];
+
+        for (int i = 1; i<=n; i++) {
+            dist[i] = (int)1e9;
+        }
+
+        pq.add(new Element(0, a));
+
+        dist[a] = 0;
+
+        while (!pq.isEmpty()) {
+            Element e = pq.poll();
+
+            int minDist = e.dist;
+            int minIndex = e.index;
+
+            if (minDist != dist[minIndex]) {
+                continue;
+            }
+
+            for (int i = 0; i<graph[minIndex].size(); i++) {
+                int targetIndex = graph[minIndex].get(i).index;
+                int targetDist = graph[minIndex].get(i).dist;
+
+                if (dist[targetIndex] > dist[minIndex] + targetDist) {
+                    dist[targetIndex] = dist[minIndex] + targetDist;
+                    pq.add(new Element(dist[minIndex] + targetDist, targetIndex));
+                }
+            }
+        }
+
+        System.out.print(dist[b]);
+    }
+}
