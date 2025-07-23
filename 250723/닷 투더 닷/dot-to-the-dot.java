349f092bb6ed97f786272aa26d9d8e4f323dff28
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-07-23 22:58:28 +0900

    Title: 닷 투더 닷, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250723/닷 투더 닷/dot-to-the-dot.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+class Node {
+    int index;
+    int L;
+    int C;
+
+    public Node (int index, int L, int C) {
+        this.index = index;
+        this.C = C;
+        this.L = L;
+    }
+}
+
+class Element implements Comparable<Element> {
+    int A;
+    int B;
+    int time;
+    int index;
+
+    public Element (int A, int B, int time, int index) {
+        this.A = A;
+        this.B = B;
+        this.time = time;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo (Element e) {
+        return this.time - e.time;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int x = sc.nextInt();
+
+        ArrayList<Node>[] graph = new ArrayList[n+1];
+
+        for (int i = 1; i<=n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int I = sc.nextInt();
+            int J = sc.nextInt();
+            int L = sc.nextInt();
+            int C = sc.nextInt();
+
+            graph[I].add(new Node(J, L, C));
+            graph[J].add(new Node(I, L, C));
+        }
+
+        PriorityQueue<Element> pq = new PriorityQueue<>();
+
+        int[] time = new int[n+1];
+
+        for (int i = 1; i<=n; i++) {
+            time[i] = (int)1e9;
+        }
+
+        time[1] = 0;
+
+        pq.add(new Element(Integer.MAX_VALUE, 0, 0, 1));
+
+        while (!pq.isEmpty()) {
+            Element e = pq.poll();
+
+            int A = e.A;
+            int B = e.B;
+            int minTime = e.time;
+            int minIndex = e.index;
+
+            if (minTime != time[minIndex]) {
+                continue;
+            }
+
+            for (int i = 0; i<graph[minIndex].size(); i++) {
+                int L = graph[minIndex].get(i).L;
+                int C = graph[minIndex].get(i).C;
+                int targetIndex = graph[minIndex].get(i).index;
+
+                int p = Math.min(A, C);
+                int q = B + L;
+
+                int newTime = q + x / p;
+
+                if (time[targetIndex] > newTime) {
+                    time[targetIndex] = newTime;
+
+                    pq.add(new Element(p, q, newTime, targetIndex));
+                }
+            }
+        }
+
+        System.out.print(time[n]);
+    }
+}
