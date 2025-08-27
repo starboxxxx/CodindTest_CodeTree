c94fef027b16531d13a321860ab25b2f39e7a78d
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-08-27 12:13:08 +0900

    Title: 점수 비교, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250827/점수 비교/score-comparison.py
+++ 250827/점수 비교/score-comparison.py
@@ -1,1 +1,7 @@
-a,b = map(int, input().split())
+A_math, A_Eng = map(int, input().split())
+B_math, B_Eng = map(int, input().split())
+
+if A_math > B_math and A_Eng > B_Eng:
+    print(1)
+else:
+    print(0)
