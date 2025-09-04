65c413ec8d441115b81d96e8769c7f96897561d6
Andrew <98813748+starboxxxx@users.noreply.github.com>
2025-09-04 15:42:38 +0900

    Title: 100점 도달하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/100점 도달하기/reach-100-point.py
+++ 250904/100점 도달하기/reach-100-point.py
@@ -5,13 +5,13 @@ for i in range(100):
     i = num + i
     if i > 100:
         break
-    elif 100 >= i >= 90:
+    elif i >= 90:
         print("A", end = " ")
-    elif 90 > i >= 80:
+    elif i >= 80:
         print("B", end = " ")
-    elif 80 > i >= 70:
+    elif i >= 70:
         print("C", end = " ")
-    elif 70 > i >= 60:
+    elif i >= 60:
         print("D", end = " ")
     elif i < 60:
         print("F", end = " ")
